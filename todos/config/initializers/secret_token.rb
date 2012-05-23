49486e8c49375fda772b2ce9c153e731a30f5ec6
Sterling Cobb <sterling@apprentice.io>
2012-05-23 13:27:15 -0400

    Moved files out of todo

--- todos/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Todos::Application.config.secret_token = '9d9fe02d5a0c9c3fc17c72e21a50444e1e185d42bca6f777ce213e271d41912b0e383d3d909ed93046990b190f3e1ff910745490c9f6e8cba0e8b42847617c16'
